8179c96f7e2b13367aa3da68ccd74976ab3eecf5
safi1997 <safollah.karimi@gmail.com>
2023-02-16 04:27:25 +0100

    The Assignment is not ready solved

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="col-md-12 text-center">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
